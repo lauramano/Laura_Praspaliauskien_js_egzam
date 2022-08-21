e250fac74247ccbd17f4db579327a98d246d2dae
laurapras <83669900+laurapras@users.noreply.github.com>
2022-08-22 01:18:51 +0300

    sesta uzd

--- JS Tasks/Task 6/script.js
+++ JS Tasks/Task 6/script.js
@@ -19,3 +19,16 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+// ================= 1
+const getUserAverageAge = users.reduce((totalAge, uObj) => totalAge + uObj.age, 0) / users.length;
+console.log('users masyvo amziaus vidurkis:', getUserAverageAge.toFixed(2));
+
+// ====================2
+
+function  getUsersNames(arr, prop) {
+  let usersNames = arr.map(item => item[prop]);
+  return usersNames;
+}
+const rezult = getUsersNames(users, 'name');
+console.log('Users masyvo objektu vardai:', rezult);
