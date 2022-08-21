1c10c7e1d5583ebfd02d4495ba31a8aa266b4b6c
laurapras <83669900+laurapras@users.noreply.github.com>
2022-08-22 02:14:13 +0300

    septinta uzd

--- JS Tasks/Task 7/script.js
+++ JS Tasks/Task 7/script.js
@@ -10,3 +10,9 @@ const audi = {
   year: 2005,
   color: 'white',
 };
+
+const showObjectKeys = (audi) => {
+ return Object.entries(audi).map(item =>item[0]);
+};
+
+console.log('audi objekto key masyve :',showObjectKeys(audi));
