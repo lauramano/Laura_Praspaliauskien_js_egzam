a6a12d923c25ea67cc22c8a6299b2022f9741381
laurapras <83669900+laurapras@users.noreply.github.com>
2022-08-22 01:51:03 +0300

    pirma uzduotis

--- JS Tasks/Task 1/script.js
+++ JS Tasks/Task 1/script.js
@@ -8,3 +8,21 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+'use strict';
+
+const svorisKg = prompt('iveskite svori kilogramais');
+document.querySelector('form').addEventListener('submit-btn', (e) =>{
+    const searchEl = document.getElementById('search');
+    const svorisKg = Number(searchEl.textContent);
+});
+// console.log('Svoris kilogramais:', svorisKg);
+
+const svorisSvarais = svorisKg * 2.2046;
+const svorisGramais = svorisKg / 0.001;
+const svorisUncijomis = svorisKg * 35.274
+
+const svorioSkaicEl = document.getElementById('output');
+svorioSkaicEl.innerHTML =
+`<p> Svoris svarais : ${svorisSvarais.toFixed(2)} svarai</p>
+<p> Svoris gramais : ${svorisGramais.toFixed(2)} gramai</p>
+<p> Svoris uncijomis : ${svorisUncijomis.toFixed(2)} uncijos</p> `;
