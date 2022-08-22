a9f416568ea2d2c6d2f3c0d23f0d33caecbb2788
laurapras <83669900+laurapras@users.noreply.github.com>
2022-08-22 05:14:49 +0300

    last

--- JS Tasks/Task 10/modules/numbers/numbers.js
+++ JS Tasks/Task 10/modules/numbers/numbers.js
@@ -1,5 +1,4 @@
-let one = 1;
-let two = 2;
-let three = 3;
-let four = 4;
-let five = 5;
+
+
+
+export let one = 1, two = 2, three = 3, four = 4, five = 5;
--- JS Tasks/Task 10/script.js
+++ JS Tasks/Task 10/script.js
@@ -1,8 +1,13 @@
 /* ------------------------------ TASK 10 ---------------------------------------------------
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
 -------------------------------------------------------------------------------------------- */
+'use strict';
+// import {numbers} from './modules/numbers/numbers.js';
+// import {composition} from './modules/math/composition.js';
 
-let a = composition(one, four);
+
+
+let a = composition(1, 4);
 let b = division(four, two);
 let c = substraction(three, two);
 let d = multiplication(five, two);
